11b09d8b422f3925044f58d51021b05f8b085ac7
papatomAS196 <thomaschisnall@garincollege.nz>
2025-03-24 01:01:51 +0000

    cricket

---
+++ script.js
@@ -0,0 +1,32 @@
+AFRAME.registerComponent('videoplayer', {
+
+init: function () {
+
+let videosource = document.querySelector('#videoclip');
+
+let videoplay = () => {
+
+videosource.play();
+
+}
+
+this.el.addEventListener('click', videoplay);
+
+}});
+
+AFRAME.registerComponent('videopauser', {
+
+init: function () {
+
+let videosource = document.querySelector('#videoclip');
+
+let videopause = () => {
+
+videosource.pause();
+
+}
+
+this.el.addEventListener('click', videopause);
+
+}});
+
